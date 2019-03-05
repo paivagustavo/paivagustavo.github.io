b601c58495635b2c41fb77cfe48eddfe4195ff48
Gustavo Paiva <guustavo.paiva@gmail.com>
2019-03-05 19:34:16 -0300

    Updates

--- src/Resume.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Resume from "./js/Resume";
-
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<Resume />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
